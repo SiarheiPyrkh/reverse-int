37d0f79599fe9865597af9c85b16fd0652cff267
Siarhei Pyrkh <stshnik@mail.ru>
2022-02-12 17:10:39 +0300

    docs: add task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
+    let arr = [];
   
+    for (let i = String(n).length -1; i >= 0; i--) {
+        arr.push(String(n)[i]);
+    }
+
+    return(parseInt(arr.join('')));
 }
